818065bec4c0909aa865b056aa8ea03a7cd6731f
YOJU-e <111264176+YOJU-e@users.noreply.github.com>
2024-08-15 09:59:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def crawling():
         )
     options = Options()
     options.add_argument("--disable-gpu")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     
